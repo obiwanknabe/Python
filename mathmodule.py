aa97166774e84ba9cd8da248cf42e744408f9de5
obiwanknabe <59886076+obiwanknabe@users.noreply.github.com>
2020-05-01 10:20:11 -0400

    Python tutorial exercise: exploring math module

---
+++ mathmodule.py
@@ -0,0 +1,12 @@
+import math
+x = 2.9
+print(math.ceil(x))
+y = 1.50
+print(math.floor(y))
+
+print(math.copysign( 2.0, -7.0))
+print(math.gcd(8, 10))
+print(math.isnan(x))
+print(math.log10(3))
+print(math.pow(2,3)
+  
