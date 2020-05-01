498ff43a029d93040de4228c2799b1776e66ed00
obiwanknabe <59886076+obiwanknabe@users.noreply.github.com>
2020-05-01 10:37:37 -0400

    Python tutorial exercise with if/else statements

---
+++ conditions.py
@@ -0,0 +1,27 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water!")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes!")
+else:
+    print("It's a lovely day")
+
+print("Enjoy your day.")
+
+
+home_price = 1000000
+credit_is_good = True
+credit_is_bad = False
+
+if credit_is_good:
+    down_payment = 0.1 * home_price
+
+elif credit_is_bad:
+    down_payment = 0.2 * price
+print(f"Down payment: ${down_payment}")
+
+
